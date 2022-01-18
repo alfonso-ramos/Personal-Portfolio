a4d23097a2efd85e28141a55474220ca734455da
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-01-17 19:08:47 -0700

    Agregamos al proyecto SCSS y automatizaciones con gulp, pequeñas modificaciones al apartado del index, Solucion del error en el 'Sobre mi' al desplegar la imagen y falta de espacio

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// Compilador de SASS
+const { src, dest, watch, parallel } = require('gulp');
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber')
+//Imagenes
+const webp = require('gulp-webp')
+function css(fn) {
+    // Identificar los archivos sass
+    src('src/scss/**/*.scss')
+        .pipe(plumber())
+    //Compilar
+        .pipe(sass())
+    //Almacenar
+        .pipe(dest('build/css'))
+    fn();
+}
+function versionWebp(fn){
+
+    const opciones = {
+        quality:50
+    }
+
+    src('src/img/**/*.{jpg,png}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    fn();
+}
+function dev(fn) {
+    watch('src/scss/**/*.scss', css);
+
+    fn();
+}
+
+
+exports.css = css;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
+
+
